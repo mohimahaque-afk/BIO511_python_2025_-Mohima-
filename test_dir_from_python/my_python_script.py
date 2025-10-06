40c3bb8a6a98e2ba438201ca05c11bcd82fdb30d
mohimahaque-afk <mohima.haque@gmail.com>
2025-10-06 15:46:44 +0200

    Adding Practical 0 directory

---
+++ test_dir_from_python/my_python_script.py
@@ -0,0 +1,10 @@
+# print("Hello, World!")
+import os
+import sys
+# # Save the path to a variable called my_file_path
+# my_file_path = 'test_dir_from_python/subdir1/my_python_script.py'
+# # Get only the file name
+# os.path.basename(my_file_path)
+
+input_string = sys.argv[1]
+print(f"Input string: {input_string}")
---
+++ test_dir_from_python/subdir2/input_arguments_test.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+input_string = os.path.basename('test_dir_from_python/subdir2/input_arguments_test.py')
+
+print(f"Input string: {input_string}")
